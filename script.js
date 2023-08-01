9fac3b84ab7d43f976be498460a4cc4b01065151
biggerpipicek <96359292+biggerpipicek@users.noreply.github.com>
2023-08-01 23:35:38 +0200

    changed some things in the HTML and JS files

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 //MAIN VARIABLES
-var color_picker = document.getElementById("chosen_color");
-var color_range = document.getElementById("color_palette");
+var color_picker = document.getElementById("colorPicker");
+var color_palette = document.getElementById("colorPalette");
 var hex_code = document.getElementById("hex_code");
-var input_range = document.getElementById("color_range");
+var color_slider = document.getElementById("colorSlider");
 
 //START THE COLOR PICKER FROM BLACK (#0000) AND 0, 0
 var chosen_color = "#000000";
-var clr_range = 0;
-var inp_range = 0;
+var color_range = 0;
+var input_range = 0;
 
 //EVENTS
 color_picker.addEventListener("input", updateColor);
-color_range.addEventListener("input", updateColorSlider);
+color_slider.addEventListener("input", updateColorSlider);
 
 //FUNCTION TO UPDATE THE COLOR AND RELATED ELEMENTS
 function updateColor() {
     chosen_color = color_picker.value;
-    color_range.style.backgroundColor = chosen_color;
-    hex_code.innerHTML = chosen_color;
+    color_palette.style.backgroundColor = chosen_color;
+    hex_code.textContent = chosen_color;
 }
 
 //FUNCTION TO UPDATE THE COLOR RANGE AND COLOR_PICKER VALUE
 function updateColorSlider() {
-    inp_range = color_range.value;
-    clr_range = Math.floor(inp_range * 2.55); //CONVERTING THE RANGE (0-255) TO COLOR VALUE
-    chosen_color = `rgb(${clr_range}, ${clr_range}, ${clr_range})`;
+    inp_range = color_slider.value;
+    color_range = Math.floor(inp_range * 2.55); //CONVERTING THE RANGE (0-255) TO COLOR VALUE
+    chosen_color = `rgb(${color_range}, ${color_range}, ${color_range})`;
     color_picker.value = chosen_color;
-    color_range.style.backgroundColor = chosen_color;
-    hex_code.innerHTML = chosen_color;
+    color_palette.style.backgroundColor = chosen_color;
+    hex_code.textContent = chosen_color;
 }
